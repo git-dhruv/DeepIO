a29dffaf0c292c5671243d4a8cb99ebb228e3dba
Anirudh Kailaje <kailaje@seas.upenn.edu>
2023-04-21 18:54:02 -0400

    homogenizeData to merge all sensor data

--- src/dataloader.py
+++ src/dataloader.py
@@ -6,54 +6,60 @@
 
 import numpy as np
 import pandas as pd
-import os, sys
+import os
+import sys
 
 from bagpy import bagreader
 import logging
 from os.path import join
 from tqdm import tqdm
 
+
 class dataloader:
 
-    def __init__(self,location):
-        self.folder = os.path.abspath(os.path.expanduser(os.path.expandvars(location)))
+    def __init__(self, location):
+        self.folder = os.path.abspath(
+            os.path.expanduser(os.path.expandvars(location)))
         self.cases = os.listdir(self.folder)
         self.imu_data = None
         self.rotor_data = None
         self.mocap_data = None
-        self.setupLogging() 
-        logging.info("Dataloader Started")       
+        self.setupLogging()
+        logging.info("Dataloader Started")
 
     def setupLogging(self):
         log_format = "[%(filename)s]%(lineno)d::%(message)s"
         logging.basicConfig(level='DEBUG', format=log_format)
-    
+
     def loadCase(self, case):
-        for folder in os.listdir(os.path.join(self.folder,case)):
+        for folder in os.listdir(os.path.join(self.folder, case)):
             logging.info(f"Loading {folder}")
-            folder = join(join(self.folder,case),folder)
-            bagfile = os.path.join(folder,"rosbag.bag")
-            imu,rotor,mocap = self.parseBagFile(bagfile)
+            folder = join(join(self.folder, case), folder)
+            bagfile = os.path.join(folder, "rosbag.bag")
+            imu, rotor, mocap = self.parseBagFile(bagfile)
             if self.imu_data is None:
                 self.imu_data = imu
                 self.rotor_data = rotor
                 self.mocap_data = mocap
             else:
-                self.imu_data = pd.concat([self.imu_data, imu], ignore_index=True, axis=0)
-                self.rotor_data = pd.concat([self.rotor_data, rotor], ignore_index=True, axis=0)
-                self.mocap_data = pd.concat([self.mocap_data, mocap], ignore_index=True, axis=0)
+                self.imu_data = pd.concat(
+                    [self.imu_data, imu], ignore_index=True, axis=0)
+                self.rotor_data = pd.concat(
+                    [self.rotor_data, rotor], ignore_index=True, axis=0)
+                self.mocap_data = pd.concat(
+                    [self.mocap_data, mocap], ignore_index=True, axis=0)
 
     def parseBagFile(self, file):
         imu_topic = '/blackbird/imu'
         rpm_topic = '/blackbird/rotor_rpm'
         mocap_topic = '/blackbird/state'
-        relevant_topics = [imu_topic,rpm_topic,mocap_topic]
+        relevant_topics = [imu_topic, rpm_topic, mocap_topic]
 
-        relevant_headers = [['header.stamp.nsecs', 'angular_velocity.x','angular_velocity.y',
-                             'angular_velocity.z','linear_acceleration.x','linear_acceleration.y','linear_acceleration.z'],
-                             ['header.stamp.nsecs','rpm_0','rpm_1','rpm_2','rpm_3'],
-                             ['header.stamp.nsecs','pose.position.x','pose.position.y','pose.position.z','pose.orientation.x','pose.orientation.y','pose.orientation.z',
-                              'pose.orientation.w']]
+        relevant_headers = [['Time', 'angular_velocity.x', 'angular_velocity.y',
+                             'angular_velocity.z', 'linear_acceleration.x', 'linear_acceleration.y', 'linear_acceleration.z'],
+                            ['Time', 'rpm_0', 'rpm_1', 'rpm_2', 'rpm_3'],
+                            ['Time', 'pose.position.x', 'pose.position.y', 'pose.position.z', 'pose.orientation.x', 'pose.orientation.y', 'pose.orientation.z',
+                             'pose.orientation.w']]
         csvfiles = []
 
         bagrdr = bagreader(file)
@@ -63,17 +69,31 @@ class dataloader:
                 csvfiles.append(data)
         imu = pd.read_csv(csvfiles[0])
         rotor = pd.read_csv(csvfiles[1])
-        mocap = pd.read_csv(csvfiles[2])        
-        imu,rotor,mocap = imu[relevant_headers[0]],rotor[relevant_headers[1]],mocap[relevant_headers[2]]
-        return imu,rotor,mocap
+        mocap = pd.read_csv(csvfiles[2])
+        imu, rotor, mocap = imu[relevant_headers[0]
+                                ], rotor[relevant_headers[1]], mocap[relevant_headers[2]]
+        return imu, rotor, mocap
 
-                    
     def runPipeline(self):
         for case in tqdm(self.cases):
             self.loadCase(case)
-        return self.imu_data,self.rotor_data,self.mocap_data
-            
-        
+        return self.imu_data, self.rotor_data, self.mocap_data
+
+    def homogenizeData(self):
+        """
+        This function will homogenize the data by interpolating the data, and creating a single dataframe. 
+        Key operations: set Time as index, merge all dataframes, interpolate data with 'linear' method and fill na with the first upcoming reading.
+        """
+        imucopy = self.imu_data.copy()
+        rotorcopy = self.rotor_data.copy()
+        mocapcopy = self.mocap_data.copy()
+        imucopy.set_index('Time', inplace=True)
+        rotorcopy.set_index('Time', inplace=True)
+        mocapcopy.set_index('Time', inplace=True)
+        self.ConcatData = imucopy.join(
+            rotorcopy, how='outer').join(mocapcopy, how='outer')
+        self.ConcatData.interpolate(method='linear', inplace=True)
+        self.ConcatData.fillna(method='bfill', inplace=True)
 
 
 if __name__ == "__main__":
