5ee763c581323986f01b3400da229dbf35d3c7de
Dhruv Parikh <dhruvparikhrg@gmail.com>
2023-04-24 14:16:30 -0400

    added dataloader

--- src/mocapFusion.py
+++ src/mocapFusion.py
@@ -13,9 +13,9 @@ class OnlineLearingFusion:
         """
         Standard Multi Sensor Fusion Parameters
         """
-        self.state = np.zeros(16,1)
-        self.covariance = np.zeros(15,15)
-        self.Q = np.zeros(7,7)
+        self.state = np.zeros((16,1))
+        self.covariance = np.zeros((16,16))
+        self.Q = np.zeros((7,7))
         self.R = np.zeros_like(self.covariance)
 
         self.PropogationJacobian = None
@@ -31,14 +31,23 @@ class OnlineLearingFusion:
         """
     def calcJacobian(self, dt, measurment=1):
         if measurment:
+            # self.MeasurmentJacobian = 
             return self.MeasurmentJacobian
         else:
+            self.PropogationJacobian = np.array([[],
+                                                 [],
+                                                 [],
+                                                 [],
+                                                 [],
+                                                 [],
+                                                 [],
+                                                 []])
             return self.PropogationJacobian
         
     def propogateStep(self, state,rpm,dt):
-        J = self.calcJacobian(dt,measurment=0)
+        self.calcJacobian(dt,measurment=0)
         self.state = self.dynamics.propogateDynamics(state,rpm, dt)
-        self.covariance = J@self.covariance@J.T + self.R
+        self.covariance = self.PropogationJacobian@self.covariance@self.PropogationJacobian.T + self.R
 
     def measurmentStep(self, measurments, dt):
         G = self.calcJacobian(dt,measurment=1)
@@ -46,6 +55,14 @@ class OnlineLearingFusion:
     
     def runPipeline(self):
         #1. Load the Data
+        loadDataUtil = dataloader("../data/clover")
+        loadDataUtil.runPipeline()
+        loadDataUtil.homogenizeData()
+        data = loadDataUtil.ConcatData
+        #R from imu to ned Frame
+        R_imu_to_ned = np.array([[-1,0,0],
+                                [0,-1,0],
+                                [0,0,1]])
         #2. Run through the Loop
             #3. Propogate Step
             #4. Measurement Update
@@ -54,3 +71,6 @@ class OnlineLearingFusion:
     
     def plotSampleOutput(self):
         raise NotImplementedError
+if __name__ == '__main__':
+    learner = OnlineLearingFusion()
+    learner.runPipeline()
