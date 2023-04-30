ee9fff37086e9b5865719b659df9df882f3d6601
Dhruv Parikh <dhruvparikhrg@gmail.com>
2023-04-30 16:07:22 -0400

    batched training

--- src/learner.py
+++ src/learner.py
@@ -20,34 +20,7 @@ from torch.nn import LSTM
 
 # os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-
-class Model(nn.Module):
-    def __init__(self, device):
-        super().__init__()   
-        #Input -> g3 ac3 and previous angles -> 3 + dt    -> 10
-        self.fc1 = nn.Linear(10, 128, bias=False)
-        # Added LSTM
-        self.lstm = LSTM(input_size=128, hidden_size=48,
-                         num_layers=5, dropout=0.3)
-        self.fc3 = nn.Linear(48, 30, bias=False)
-        self.fc4 = nn.Linear(30,3)
-        # Added another FC Layer
-
-    def forward(self, x):
-        # measurementPacket,eulers[i-1,:],dt
-        x = self.fc1(x)
-        x = F.leaky_relu(x)
-
-        x = x.unsqueeze(0)
-        # LSTM
-        x, _ = self.lstm(x)
-        x = x.view(-1, 1).flatten()
-
-        x = self.fc3(x)
-        x = F.sigmoid(x)
-        x = self.fc4(x)
-        
-        return x
+from imuModel import Model
 
 
 
@@ -97,50 +70,184 @@ writer = SummaryWriter(workspace)
 
 print('[SETUP] Establishing model and optimizer.')
 model = Model(device).to(device)
-# model = ConvNet(device=device)
-
 # optimizer = torch.optim.SGD(model.parameters(), lr=5e-6, momentum=0.9)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-6)
+optimizer = torch.optim.Adam(model.parameters(), lr=8e-4)
+# optimizer = torch.optim.Adadelta(params=model.parameters(), lr=1e-3)
 
 # model.load_state_dict(torch.load("C:\Dhruv Personal Files\RL\\agile_flight_dev_env\\agile_flight\model_120.pth"))
+model.load_state_dict(torch.load(r"C:\Users\dhruv\Desktop\Penn\Sem2\ESE650\FinalProject\DeepIO\model.pth"))
 
-N_eps = 4
+N_eps = 10000
 itrs = 0
 print(f'[TRAIN] Training for {N_eps} epochs.')
 total_itr = 0
-# eulers = torch.tensor(eulers).to(device)
-for ep in range(N_eps):
-    ep_loss = 0
-    path = ""
-    torch.save(model.state_dict(), path+f"model_{ep}.pth")
-    for i in tqdm.tqdm(range(1,t.shape[0]-1000)):
-        dt = t[i] - t[i-1]
-        measurementPacket = np.array([float(acc[0,i]),float(acc[1,i]),float(acc[2,i]),
-                                            float(gyro[0,i]),float(gyro[1,i]),float(gyro[2,i]),
-                                            float(eulers[i-1,0]),
-                                            float(eulers[i-1,1]),
-                                            float(eulers[i-1,2]),
-                                            float(dt)])
-        measurementPacket = torch.tensor(measurementPacket).to(device)
-        
-        optimizer.zero_grad()
-        pred = model(measurementPacket.float())
-        loss = F.mse_loss(pred, torch.tensor(eulers[i,:]).to(device).float())
-        ep_loss += loss
 
+
+
+    #### NODE #####
+"""
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchdiffeq import odeint_adjoint as odeint
+import numpy as np
+import matplotlib.pyplot as plt
+
+method = 'dopri5'
+batch_time = 15
+batch_size = TrainSize-100
+niters = 2000
+test_freq = 10
+viz = 'store_true'
+adjoint = 'store_true'
+
+def mini_batch(yTrain, xTrain, size, batch_size, batch_time):
+        
+    s = torch.from_numpy(np.random.choice(np.arange(size-batch_time, dtype=np.int64), batch_size, replace=False))
+    batch_y0 = xTrain[s,:4]
+    batch_x = xTrain[:batch_time,4] 
+    batch_y = torch.stack([torch.Tensor(xTrain[s + i,:4]) for i in range(batch_time)], dim=0)
+    batch_yy = yTrain[s,:]
+    batch_yy1 = yTrain[s+1,:]
+
+    return batch_y0, batch_x, batch_y, batch_yy, batch_yy1
+
+def TrainNODENetwork(yTrain, xTrain, size, batch_size, batch_time, FuncControl, FuncDynamics, niters=10000,):
+
+    parameters = list(FuncControl.parameters()) + list(FuncDynamics.parameters())
+    #optimizerD = optim.Adam(params=parameters, lr=1e-3)
+    optimizer = optim.Adadelta(params=parameters, lr=1.0, rho=0.9, eps=1e-06, weight_decay=0) #
+
+    for itr in range(1,niters +1):
+        optimizer.zero_grad()    
+        batch_y0, batch_x, batch_y, batch_yy, batch_yy1 = mini_batch(yTrain, xTrain, size, batch_size, batch_time)
+        pred_y = odeint(FuncControl, torch.Tensor(batch_y0), torch.Tensor(batch_x))
+        
+        XDynamics = torch.cat((torch.Tensor(batch_y0), torch.Tensor(batch_yy)), 1) #pred_y[1,:,:]
+        pred_yy = FuncDynamics(XDynamics)
+        lossD = torch.mean(torch.abs(pred_yy - torch.Tensor(batch_yy1)))
+        #lossC = torch.mean(torch.abs(pred_y - torch.Tensor(batch_y)))
+
+        loss = lossD# + lossC
         loss.backward()
         optimizer.step()
 
-        if i % 20000 == 0:
-            tqdm_text = f'Loss = {loss:4f}'
-            print(tqdm_text)
-            # tqdm.write(tqdm_text)
-        itrs = itrs+1
+        if itr % test_freq == 0:
+            '''with torch.no_grad():
+                pred_y = odeint(Func, torch.Tensor(y0[1,:]), t)
+                loss = torch.norm(pred_y - torch.Tensor(y[:,1,:]))'''
+            print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
+    
+    return FuncControl, FuncDynamics
 
-    if total_itr == 0:
-        total_itr = itrs
+class ODEFunc(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(4, 30),
+            nn.ReLU(),
+            nn.Linear(30, 30),
+            nn.ReLU(),
+            nn.Linear(30, 4),
+        )
+        self.net = self.net.float()
+        self.net.apply(self._apply_wt_init)
 
-    ep_loss /= total_itr
+    def forward(self, t, y):
+        return self.net(y)
+    
+    def _apply_wt_init(self, layer):
+        if isinstance(layer, nn.Linear):
+            nn.init.normal_(layer.weight, mean=0, std=0.1)
+            nn.init.constant_(layer.bias, val=0)
+
+class Func2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(10, 35),
+            nn.Tanh(),
+            nn.Linear(35, 35),
+            nn.Tanh(),
+            nn.Linear(35, 6),
+        )
+        self.net = self.net.float()
+        self.net.apply(self._apply_wt_init)
+
+    def forward(self, y):
+        return self.net(y)
+    
+    def _apply_wt_init(self, layer):
+        if isinstance(layer, nn.Linear):
+            nn.init.normal_(layer.weight, mean=0, std=0.1)
+            nn.init.constant_(layer.bias, val=0)
+
+FuncControl = ODEFunc()
+FuncDynamics = Func2()
+
+FuncControl, FuncDynamics = TrainNODENetwork(train_y, train_x, TrainSize, batch_size, batch_time, FuncControl, FuncDynamics)
+
+
+
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+size = 100000
+
+TrainSize = 80000
+batch_time = 15
+batch_size = TrainSize-100
+
+eulers = torch.tensor(eulers).to(device)
+measurements = np.vstack((acc,gyro))
+measurements = torch.tensor(measurements).to(device)
+for ep in range(N_eps):
+    ep_loss = 0
+    if ep%100==0:
+        path = ""
+        torch.save(model.state_dict(), path+f"model_{ep}.pth")
+    
+    # for i in tqdm.tqdm(range(500,5000)):
+        # if i%20!=0:
+        #     continue
+    s = torch.from_numpy(np.random.choice(np.arange(size-batch_time, dtype=np.int64), batch_size, replace=False))
+    
+    batch_x = (measurements[:,s].T).float()
+    batch_y = eulers[s,:2].unsqueeze(0).float()
+
+    # dt = t[i] - t[i-1]
+    # acc[:,i] = acc[:,i]/np.linalg.norm(acc[:,i])
+    # measurementPacket = np.array([float(acc[0,i]),float(acc[1,i]),float(acc[2,i]),
+    #                                     float(gyro[0,i]),float(gyro[1,i]),float(gyro[2,i]),
+    #                                     float(dt)])
+    # measurementPacket = torch.tensor(measurementPacket).to(device)
+    optimizer.zero_grad()
+    
+    pred = model(batch_x)
+    loss = F.mse_loss(pred, batch_y)
+    ep_loss = loss
+
+    loss.backward()
+    optimizer.step()
 
     print(f'[TRAIN] Completed ep {ep}/{N_eps-1}, ep_loss = {ep_loss:.6f}')
 
