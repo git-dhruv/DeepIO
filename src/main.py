1c9639d81bdc99e8ca186a049c10ae4984f81888
Anirudh Kailaje <kailaje@seas.upenn.edu>
2023-05-02 21:56:55 -0400

    Average phenomenon

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import MinMaxScaler
 from torchdiffeq import odeint
 from Training import CreateTrainingData
 
-PATH = "TrainedModels/save.pt"
+PATH = r'C:\Users\aniru\Documents\01_UPenn\04_ESE6500\02_Homework\05_Project\DeepIO\src\model.pkl'
 
 # Invoking saved model
 
@@ -21,9 +21,9 @@ optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 epoch = checkpoint['epoch']
 loss = checkpoint['loss']
 
-model.eval(True)
+model.eval()
 
-tmp = dataloader("data/Oval/")
+tmp = dataloader("data/Oval/",0)
 imu, rotor, mocap = tmp.runPipeline()
 tmp.homogenizeData()
 dataFrame = tmp.ConcatData
@@ -38,13 +38,14 @@ ax = plt.figure().add_subplot(projection='3d')
 ax.plot(yTrain[:,0],yTrain[:,2],yTrain[:,4], color='red')
 
 scaler_X = MinMaxScaler(feature_range=(0, 1))
-
+y0 = yTrain[0,:]
 for i in range(0,size-batch_time):
-    y0 = yTrain[i,:]
+    # y0 = yTrain[i,:]
     pred_u = odeint(model, torch.Tensor(y0.flatten()), torch.Tensor(scaler_X.fit_transform(xTrain[i:i+batch_time]).flatten()))
     pu = pred_u.detach().numpy()
     y0 =  scaler_X.fit_transform(pu[1,:].reshape(-1,1))
     ax.scatter(y0[0], y0[2],y0[4], s=2,c='b')
+    # y0 = mocap[i,:].flatten()
 
 plt.show()
 
